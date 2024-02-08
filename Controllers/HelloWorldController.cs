8c28c2cf340fb7fdcdbe7fd158934be80ee9b91c
Lucas Breno de Souza Noronha Braga <lucasbbs@live.fr>
2024-02-08 13:00:55 -0500

    Provide some inline documentation

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -9,15 +9,27 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
+
+        /*
+         * This method is responsible for handling HTTP GET requests to display a welcome message.
+         * It accepts two parameters: 'name' (string) and 'numTimes' (int, optional with a default value of 1).
+         * The method performs the following operations:
+         * 1. Sets ViewData variables to store the 'name' and 'numTimes' parameters.
+         * 2. Constructs a welcome message using the provided 'name'.
+         * 3. Returns a View for displaying the welcome message along with the number of times to display it.
+         */
         public IActionResult Welcome(string name, int numTimes = 1)
         {
-            // return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
-            // return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
             ViewData["Message"] = "Hello " + name;
             ViewData["NumTimes"] = numTimes;
             return View();
         }
 
+        /*
+         * This method is a custom action named 'Lucas' that returns a string.
+         * The method performs the following operation:
+         * 1. Returns a string indicating that it is Lucas' custom action.
+         */
         public string Lucas()
         {
             return "This is Lucas' custom action";
--- Models/HelloWorld.cs
+++ Models/HelloWorld.cs
@@ -35,11 +35,20 @@ namespace WebApplication1.Models
         public string Title { get; set; }
         [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        // This pattern requires the input to start with one or more capital letters,
+        // followed by zero or more letters (both uppercase and lowercase) and whitespaces.
+        // The StringLength attribute enforces a maximum length of 30 characters for the input.
         [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
         [Range(1, 100), DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+        // This pattern requires the input to start with one or more capital letters,
+        // followed by zero or more letters (both uppercase and lowercase), digits (0-9),
+        // as well as some special characters like double quotes, single quotes, spaces, and hyphens.
+        // The StringLength attribute enforces a maximum length of 5 characters for the input.
         [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$"), StringLength(5)]
         public string Rating { get; set; }
 
